8fc1733b2b5ac15dbcef3fda48536bcb6012250b
Nathan123-code <nathan6625@gmail.com>
2025-09-13 11:43:00 +0700

    minor fixes

--- backend/dokter/model_dokter.php
+++ backend/dokter/model_dokter.php
@@ -12,7 +12,7 @@ class dokter
         $this->phone = trim($phone);
         $this->email = trim($email);
         $this->note = !empty(trim($note)) ? trim($note) : "No Note";
-        $this->specialist_id = $specialist_id; //nyimpen id specialist
+        $this->specialist_id = $specialist_id; 
     }
 }
 
